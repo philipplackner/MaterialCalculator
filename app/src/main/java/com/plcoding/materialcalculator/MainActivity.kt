6cfda498061e4a0ae7a77485bfa996316d933d29
Philipp Lackner <mail@pl-coding.com>
2022-08-12 17:09:12 +0200

    Initial commit

---
+++ app/src/main/java/com/plcoding/materialcalculator/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.plcoding.materialcalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.materialcalculator.ui.theme.MaterialCalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialCalculatorTheme {
+
+            }
+        }
+    }
+}
