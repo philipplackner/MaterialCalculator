4880c93ff84f847586d133ac530071d652d2dc45
Philipp Lackner <mail@pl-coding.com>
2022-08-13 19:40:14 +0200

    Added ExpressionParser.kt

---
+++ app/src/test/java/com/plcoding/materialcalculator/domain/ExpressionParserTest.kt
@@ -0,0 +1,50 @@
+package com.plcoding.materialcalculator.domain
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class ExpressionParserTest {
+
+    private lateinit var parser: ExpressionParser
+
+    @Test
+    fun `Simple expression is properly parsed`() {
+        // 1. GIVEN
+        parser = ExpressionParser("3+5-3x4/3")
+
+        // 2. DO SOMETHING WITH WHAT'S GIVEN
+        val actual = parser.parse()
+
+        // 3. ASSERT EXPECTED == ACTUAL
+        val expected = listOf(
+            ExpressionPart.Number(3.0),
+            ExpressionPart.Op(Operation.ADD),
+            ExpressionPart.Number(5.0),
+            ExpressionPart.Op(Operation.SUBTRACT),
+            ExpressionPart.Number(3.0),
+            ExpressionPart.Op(Operation.MULTIPLY),
+            ExpressionPart.Number(4.0),
+            ExpressionPart.Op(Operation.DIVIDE),
+            ExpressionPart.Number(3.0),
+        )
+        assertThat(expected).isEqualTo(actual)
+    }
+
+    @Test
+    fun `Expression with parentheses is properly parsed`() {
+        parser = ExpressionParser("4-(4x5)")
+
+        val actual = parser.parse()
+
+        val expected = listOf(
+            ExpressionPart.Number(4.0),
+            ExpressionPart.Op(Operation.SUBTRACT),
+            ExpressionPart.Parentheses(ParenthesesType.Opening),
+            ExpressionPart.Number(4.0),
+            ExpressionPart.Op(Operation.MULTIPLY),
+            ExpressionPart.Number(5.0),
+            ExpressionPart.Parentheses(ParenthesesType.Closing),
+        )
+        assertThat(expected).isEqualTo(actual)
+    }
+}
